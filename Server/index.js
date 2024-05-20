cb2e8701921717e7c2025002c182a645ef9e6ba9
nowyouseeme1234 <issacabreham96@gmail.com>
2024-05-20 15:06:17 +0300

    added vercel.json and index.js

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,27 @@
-import express from "express"
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 import { orderRouter } from "./Routes/OrderRoutes.js";
 
-const app = express()
-app.use(cors({
-    origin: ["http://localhost:5173", "http://192.168.101.16:8080", "http://192.168.101.16:8081", 'https://mystifying-firefly-37170.pktriot.net'],
-    methods: ['GET', 'POST', 'PUT'],
-    credentials: true
-}))
+const app = express();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://192.168.101.16:8080",
+      "http://192.168.101.16:8081",
+      "https://mystifying-firefly-37170.pktriot.net",
+    ],
+    methods: ["GET", "POST", "PUT"],
+    credentials: true,
+  })
+);
+app.use("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.use(express.json())
-app.use(orderRouter)
+app.use(express.json());
+app.use(orderRouter);
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log("Server is running")
-})
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server is running");
+});

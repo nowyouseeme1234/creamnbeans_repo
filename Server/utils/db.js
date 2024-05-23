5b047bf64de9166f82363f43f79b37d24f3fb0bc
nowyouseeme1234 <issacabreham96@gmail.com>
2024-05-23 12:48:37 +0300

    test of talking to the server

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,18 +1,33 @@
 import mysql from 'mysql'
 
-
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "creamnbeansdb"
+    host: "sql12.freesqldatabase.com",
+    user: "sql12708608",
+    password: "Inj824Fg5r",
+    database: "sql12708608"
 });
 
 con.connect(function(err){
     if(err){
-        console.log("connection error")
+        console.log(`connection error to external database ${err}`)
     }
-    else console.log("Connected To CreamNbeansDB")
+    else console.log("Connected To external database sql12708608")
 })
 
 export default con;
+
+// const con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "creamnbeansdb"
+// });
+
+// con.connect(function(err){
+//     if(err){
+//         console.log("connection error")
+//     }
+//     else console.log("Connected To CreamNbeansDB")
+// })
+
+// export default con;
